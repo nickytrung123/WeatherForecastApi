04bcd82b86bb7c35721abe3f21383402279dd693
LeTrung <nickytrung123@gmail.com>
2020-12-30 16:07:53 +0700

    Add project files.

---
+++ WeatherForecastApi/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherForecastApi.Models;
+
+namespace WeatherForecastApi
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
